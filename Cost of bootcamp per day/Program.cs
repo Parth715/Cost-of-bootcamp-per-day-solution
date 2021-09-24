0b73c87bdcf08d0df45fd108d713f4efe893b238
Parth Patel <parth2002715@gmail.com>
2021-09-24 09:57:38 -0400

    Add project files.

---
+++ Cost of bootcamp per day/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Cost_of_bootcamp_per_day
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal TotalPrice = 15500;
+            int TotalDays = 65;
+            decimal PerDayCost = Math.Round(TotalPrice / TotalDays, 2);
+            Console.WriteLine($"Each day of bootcamp will cost ${PerDayCost}");
+        }
+    }
+}
